d605a9a6a7c2952fcb93427ce732a56770c93301
GregAndrik <andrikgreg@gmail.com>
2023-09-01 12:17:54 +0300

    changed about texts

--- src/components/views/about/Bio.js
+++ src/components/views/about/Bio.js
@@ -5,9 +5,9 @@ export default function Bio() {
       <p>
         My passion for programming ignited during my high school years when I was first introduced to it through my school's curriculum. Even before completing high school,
         programming had evolved into a lasting hobby that led me to envision a career path with certainty. I pursued my education at the University of Thessaly, earning a Bachelor's
-        degree in Computer Engineering. This academic journey has been complemented by continuous personal research and skill refinement. Alongside, I am enthusiastic about 
-        exploring various web technologies that capture my interest, thereby cultivating a versatile skill set to contribute effectively within development teams. My aspiration is 
-        to work with the best and learn from the best, absorbing knowledge and broadening my expertise across diverse IT domains.
+        degree in Computer Science. This academic journey has been complemented by continuous personal research and skill refinement. Alongside, I am enthusiastic about exploring 
+        various web technologies that capture my interest, thereby cultivating a versatile skill set to contribute effectively within development teams. My aspiration is to work with
+        the best and learn from the best, absorbing knowledge and broadening my expertise across diverse IT domains.
       </p>
       <h3>Soft skills:</h3>
       <p>
@@ -21,8 +21,7 @@ export default function Bio() {
         My current skill set comprises both Front-End and Back-End technologies, each of which captivates me for distinct reasons. As a junior developer, I dedicate my primary focus
         to Front-End development. However, I foresee a future where I aspire to evolve into a Fullstack developer. This direction aligns with my appreciation for both aspects of 
         software development; I'm determined not to miss out on the experience that Fullstack development offers. Moreover, I am comfortable on linux environments, while also 
-        favoring Windows' WSL2 implementation. This preference arises from the belief that it combines the best attributes of both operating systems, providing a versatile and 
-        efficient environment.
+        favoring Windows' WSL2 implementation.
       </p>
       <h3>As a colleague:</h3>
       <p>
